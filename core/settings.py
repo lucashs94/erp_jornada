86a1091084dc88e964e4afea770e1ccf39ac3f24
lucashs94 <h7.lucas@gmail.com>
2023-02-28 20:32:50 -0300

    adicionado variaveis de ambiente

--- core/settings.py
+++ core/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z1y6!+^a2adr3m39#yit^58e!&hy*=jbs7jem-__m#hkvi@^lf'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
